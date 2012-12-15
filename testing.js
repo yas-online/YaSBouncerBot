2a49d7827832616360050f55fa23e885230cd7a0
nanashiRei <nanashi.rei@gmail.com>
2012-12-15 20:15:19 +0100

    Added colors to our testsuit

--- testing.js
+++ testing.js
@@ -1,34 +1,38 @@
 var shadowDb = require("shadow-db"),
     util = require("util");
+require("colors");
 
 /** Open our shadowDb */
 var passwd = new shadowDb("mydb.json", __dirname);
 
 /** Try getting a shadow entry from a password */
 var myShadow = passwd.getShadow("29gswo85zguekulszh5go83wk4uhztog83");
-util.log( "getShadow: " + myShadow );
+util.log( "getShadow: ".yellow + myShadow );
 
 /** Reverse search on the retrieved shadow */
-util.log( "getPassPhrase: " + passwd.getPassPhrase(myShadow) );
+util.log( "getPassPhrase: ".yellow + passwd.getPassPhrase(myShadow) );
 
 /** What algorithm does our db use? */
-util.log( "getAlgorithm: " + passwd.getAlgorithm() );
+util.log( "getAlgorithm: ".yellow + passwd.getAlgorithm() );
 
 /** What's our current shadow table file? */
-util.log( passwd.loaded );
+util.log( passwd.loaded.cyan );
 
 /** Show all entries in our table */
-util.log( "Forward Table: \n" + util.inspect(passwd.forward) );
+util.log( "Forward Table: \n".yellow + util.inspect(passwd.forward, true, null, true) );
 
 
 /** Try adding a new entry to the table */
 try {
     passwd.addEntry("King Size");
-    util.log( "New key added." );
+    util.log( "New key added.".green );
 } catch (err) {
-    util.error(err);
-    util.log( "No new key added." );
+    util.log( "Error: ".red + err.toString().grey );
+    util.log( "No new key added.".red );
 }
 
 /** Save our table */
 passwd.save();
+
+/** Done. */
+util.log( "We are all ".yellow + "DONE".green );
