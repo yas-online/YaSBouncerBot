33ef7bcb77d79244c6d2a2a87dad596498088943
nanashiRei <nanashi.rei@gmail.com>
2012-12-15 19:59:42 +0100

    Added comments to our shadowDb tests

--- testing.js
+++ testing.js
@@ -1,19 +1,27 @@
 var shadowDb = require("shadow-db"),
     util = require("util");
 
+/** Open our shadowDb */
 var passwd = new shadowDb("mydb.json", __dirname);
 
+/** Try getting a shadow entry from a password */
 var myShadow = passwd.getShadow("29gswo85zguekulszh5go83wk4uhztog83");
 util.log( "getShadow: " + myShadow );
 
+/** Reverse search on the retrieved shadow */
 util.log( "getPassPhrase: " + passwd.getPassPhrase(myShadow) );
 
+/** What algorithm does our db use? */
 util.log( "getAlgorithm: " + passwd.getAlgorithm() );
 
+/** What's our current shadow table file? */
 util.log( passwd.loaded );
 
+/** Show all entries in our table */
 util.log( "Forward Table: \n" + util.inspect(passwd.forward) );
 
+
+/** Try adding a new entry to the table */
 try {
     passwd.addEntry("King Size");
     util.log( "New key added." );
@@ -22,4 +30,5 @@ try {
     util.log( "No new key added." );
 }
 
+/** Save our table */
 passwd.save();
